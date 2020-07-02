cdd95968737730f62c5f6fae8670d79b0b03e69a
Jonayah <jonayah@thinkhalcyon.com>
2020-07-02 12:42:52 -0400

    cleaning up my pseudocode and variables

--- script.js
+++ script.js
@@ -13,40 +13,33 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// PASSWORD CRITERIA
-// prompt1 is for length
-var length = prompt("How long would you like your password to be?\nPlease choose a NUMBER between 8 and 128")
-
-// "prompt2" is for character types
-alert("Do you want to include lowercase letters?\nChoose Ok for yes. Choose Cancel for no.")
-alert("Do you want to include UPPERCASE letters?\nChoose Ok for yes. Choose Cancel for no.")
-alert("Do you want to include numbers?\nChoose Ok for yes. Choose Cancel for no.")
-alert("Do you want to include special characters?\nChoose Ok for yes. Choose Cancel for no.")
-
-// variables
+// make a list of available character types
 var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
+
+// find out what the user wants
+  // length
+var pwLength = prompt("How long would you like your password to be?\nPlease choose a NUMBER between 8 and 128")
+
+  // character types
+var confirmLC = prompt("Do you want to include lowercase letters?\nType y for yes. Type n for no.");
+var confirmUC = prompt("Do you want to include UPPERCASE letters?\nType y for yes. Type n for no.");
+var confirmN = prompt("Do you want to include numbers?\nType y for yes. Type n for no.");
+var confirmS = prompt("Do you want to include special characters?\nType y for yes. Type n for no.");
+
+// make a list of accepted character types based on the chosen number
+  // string concatination
+
+
+
+// randomize the characters to make a password (string)
 var randomString = "";
 
-// FUNCTIONS and SUCH
-if (length > 0) {
-  for (var i=0; i < length; i++){
-    // need to combine lowerCase, upperCase, numbers, and specials (if user chooses)
-  }
-}
+// display characters
 
-function name(params) {
-  
-}
 
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
+// validate?
 
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
 
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
